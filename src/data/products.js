9b6a47403c9ccc68e6f65b475cd544c11649914e
Ayoub EL HERICHI <ayoub.elherichi@gmail.com>
2025-12-18 21:36:03 +0100

    Add Data

---
+++ src/data/products.js
@@ -0,0 +1,60 @@
+const products = [
+  {
+    id: "p1",
+    name: "Adidas Continental 80",
+    price: 19.99,
+    image:
+      "../../public/Adidas Continental 80.jpeg"
+  },
+  {
+    id: "p2",
+    name: "Adidas Womens Pureboost DPR",
+    price: 59.99,
+    image:
+        "../../public/Adidas Womens Pureboost DPR.jpeg"
+  },
+  {
+    id: "p3",
+    name: "adidas POD S3.1 Maroon",
+    price: 89.0,
+    image:
+            "../../public/adidas POD S3.1 Maroon.jpeg"
+  },
+  {
+    id: "p4",
+    name: "Adidas Copa Super",
+    price: 29.5,
+    image:
+        "../../public/Adidas Copa Super.jpeg"
+  },
+  {
+    id: "p5",
+    name: "Adidas x Pharrell Williams Solar Hu Glide 'Bold Gold'",
+    price: 149.99,
+    image:
+        "../../public/Adidas x Pharrell Williams Solar Hu Glide 'Bold Gold'.jpeg"
+  },
+  {
+    id: "p6",
+    name: "Nike React Element 55 'Solar Red",
+    price: 39.0,
+    image:
+        "../../public/Nike React Element 55 'Solar Red.jpeg"
+  },
+  {
+    id: "p7",
+    name: ".Adidas Womens Pureboost DPR",
+    price: 69.99,
+    image:
+        "../../public/Adidas Womens Pureboost DPR.jpeg"
+  },
+    {
+    id: "p8",
+    name: "Adidas Continental 80",
+    price: 69.99,
+    image:
+        "../../public/Adidas Continental 80.jpeg"
+  },
+];
+
+export default products;
